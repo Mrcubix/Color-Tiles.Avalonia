b9162c7784d5db028bbac2c8a6895b7586695a88
Mrcubix <tintamare91@gmail.com>
2023-09-14 00:06:59 +0200

    The status Bar on android is now transparent while using the app, providing extra space for the game

--- ColorTiles.Android/MainActivity.cs
+++ ColorTiles.Android/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
+using AndroidX.Core.View;
 using Avalonia;
 using Avalonia.Android;
 using Avalonia.ReactiveUI;
@@ -8,7 +10,7 @@ using Avalonia.ReactiveUI;
 namespace ColorTiles.Android;
 
 [Activity(
-    Label = "ColorTiles.Android",
+    Label = "Color Tiles",
     Theme = "@style/MyTheme.NoActionBar",
     Icon = "@drawable/icon",
     MainLauncher = true,
@@ -24,10 +26,53 @@ public class MainActivity : AvaloniaMainActivity<App>
 
     protected override void OnCreate(Bundle savedInstanceState)
     {
-        // force landscape orientation
-        RequestedOrientation = ScreenOrientation.Landscape;
-        
+        // Force landscape orientation
+        RequestedOrientation = ScreenOrientation.SensorLandscape;
+        // Make app fullscreen
+        Window?.AddFlags(WindowManagerFlags.Fullscreen);
+        // Hide keyboard
+        Window?.SetSoftInputMode(SoftInput.StateAlwaysHidden);
+
+        SetFullScreen();
 
         base.OnCreate(savedInstanceState);
+    }
+
+    private void SetFullScreen()
+    {
+        var sdkInt = (int)Build.VERSION.SdkInt;
+
+#pragma warning disable CA1416, CA1422
+        // Hide status bar on Android 11+
+        if (sdkInt >= 30)
+        {
+            var windowsInsetsController = Window?.InsetsController;
+
+            if (windowsInsetsController != null)
+            {
+                windowsInsetsController.SystemBarsBehavior = (int)WindowInsetsControllerBehavior.ShowTransientBarsBySwipe;
+
+                windowsInsetsController.Hide(WindowInsets.Type.StatusBars());
+            }
+        }
+        else if (sdkInt >= 21)
+        {
+            WindowCompat.SetDecorFitsSystemWindows(Window, false);
+
+            StatusBarVisibility? systemVisibility = Window?.DecorView.SystemUiVisibility;
+
+            if (systemVisibility != null)
+            {
+                var options = (int)systemVisibility;
+
+                options |= (int)SystemUiFlags.Fullscreen;
+
+                Window!.DecorView.SystemUiVisibility = (StatusBarVisibility)options;
+            }
+
+            var layoutNoLimitsFlag = WindowManagerFlags.LayoutNoLimits;
+            Window?.SetFlags(layoutNoLimitsFlag, layoutNoLimitsFlag);
+        }
+#pragma warning restore CA1416, CA1422
     }
 }

dcd430e9f9c0bb3a03090bf44baa11205f1fe91f
Mrcubix <tintamare91@gmail.com>
2023-09-24 03:12:03 +0200

    Import the javascript file and generate a different default audio set for the browser version (Not happy with the way it's done right now, could use some DI)

--- ColorTiles.Browser/Program.cs
+++ ColorTiles.Browser/Program.cs
@@ -1,19 +1,44 @@
-﻿using System.Runtime.Versioning;
+﻿using System.Runtime.InteropServices.JavaScript;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Browser;
 using Avalonia.ReactiveUI;
 using ColorTiles;
+using ColorTiles.Browser.Entities.Audio.Files;
+using ColorTiles.Entities.Audio.Sets;
+using ColorTiles.Entities.Tools.Managers;
 
 [assembly: SupportedOSPlatform("browser")]
 
 internal partial class Program
 {
-    private static async Task Main(string[] args) => await BuildAvaloniaApp()
-            .WithInterFont()
-            .UseReactiveUI()
-            .StartBrowserAppAsync("out");
+    private static async Task Main(string[] args)
+    {
+        await JSHost.ImportAsync("AudioFile.js", "./AudioFile.js");
+
+        await BuildAvaloniaApp()
+             .WithInterFont()
+             .UseReactiveUI()
+             .AfterSetup(AfterSetupCallBack)
+             .StartBrowserAppAsync("out");
+    }
 
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>();
+
+    public static void AfterSetupCallBack(AppBuilder builder)
+    {
+        if (Application.Current is not App app)
+            return;
+
+        // create the audio files
+        var buttonClickSFX = new WebAudioFile(AudioSetManager.DefaultButtonClickSFXPath);
+        var buttonHoverSFX = new WebAudioFile(AudioSetManager.DefaultButtonHoverSFXPath);
+        var matchSFX = new WebAudioFile(AudioSetManager.DefaultMatchSFXPath);
+        var penaltySFX = new WebAudioFile(AudioSetManager.DefaultPenaltySFXPath);
+        //var gameOverSFX = new WebAudioFile(AudioSetManager.DefaultGameOverSFXPath);
+
+        app.Audioset = new GameAudioSet(0, buttonClickSFX, buttonHoverSFX, matchSFX, penaltySFX, null!);
+    }
 }

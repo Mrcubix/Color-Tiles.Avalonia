ce82b9af612025fd7867d76cb5c946ede1b69468
Mrcubix <tintamare91@gmail.com>
2023-09-24 02:55:04 +0200

    - Preparations for allowing a different pair of set (Used the browser as i'm being forced to use the Interop to add Audio support)

--- ColorTiles/App.axaml.cs
+++ ColorTiles/App.axaml.cs
@@ -3,6 +3,8 @@ using System.Globalization;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using ColorTiles.Entities.Tilesets;
+using ColorTiles.Entities.Tools;
 using ColorTiles.ViewModels;
 using ColorTiles.Views;
 
@@ -10,6 +12,15 @@ namespace ColorTiles;
 
 public partial class App : Application
 {
+    public GameTileSet Tileset { get; set; }
+    public AudioSet Audioset { get; set; }
+
+    public App()
+    {
+        Tileset = null!;
+        Audioset = null!;
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -17,26 +28,26 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        //Assets.Localizations.Resources.Culture = CultureInfo.CurrentUICulture;
-
         // Check if culture is invariant
-        if(CultureInfo.CurrentCulture == CultureInfo.InvariantCulture)
+        if (CultureInfo.CurrentCulture == CultureInfo.InvariantCulture)
             Console.WriteLine("Invariant culture detected, changing localization is not supported.");
         else
             Assets.Localizations.Resources.Culture = new CultureInfo("en-US");
+
+        // Later on, check settings from file to see if a custom tileset and audioset should be loaded (if not a browser launch)
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(Tileset, Audioset)
             };
         }
         else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             singleViewPlatform.MainView = new MainView
             {
-                DataContext = new MainViewModel()
+                DataContext = new MainViewModel(Tileset, Audioset)
             };
         }
 

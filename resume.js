8f19cead2267be424d8cf62496f84941095b731c
Mrcubix <39861216+Mrcubix@users.noreply.github.com>
2023-10-02 23:29:55 +0000

    Deploying to gh-pages from @ Mrcubix/Color-Tiles.Avalonia@ad778df2a6512a39e87eedc88d215630327e51ae 🚀

--- resume.js
+++
@@ -1,9 +0,0 @@
-let resumed = false;
-
-document.addEventListener("click", () => {
-    if (!resumed) {
-        resumed = true;
-        let audioContext = new (window.AudioContext || window.webkitAudioContext)();
-        audioContext.resume();
-    }
-});
